7897a3cb19e08ed683fb5a1eec9600cd0275b44f
DMac <47114231+mcnamaradavid@users.noreply.github.com>
2019-11-12 23:00:13 -0600

    Removed big comment for how to use console colors.

--- CrypDominion/Program.cs
+++ CrypDominion/Program.cs
@@ -10,23 +10,6 @@ namespace CrypDominion
 
         public static void Main(string[] args)
         {
-            //
-            // This program demonstrates all colors and backgrounds.
-            //
-            //Type type = typeof(ConsoleColor);
-            //Console.ForegroundColor = ConsoleColor.Black;
-            //foreach (var name in Enum.GetNames(type))
-            //{
-            //    Console.BackgroundColor = (ConsoleColor)Enum.Parse(type, name);
-            //    Console.WriteLine(name);
-            //}
-            //Console.BackgroundColor = ConsoleColor.Black;
-            //foreach (var name in Enum.GetNames(type))
-            //{
-            //    Console.ForegroundColor = (ConsoleColor)Enum.Parse(type, name);
-            //    Console.WriteLine(name);
-            //}
-
             //Use cryptographically secure random number generator (CNG) to select multiple expansions for a games. Do this for as many games as desired.
             ParseParametersForAutoRun(args);
             var autoRun = new CryRanDominion(_games, _expansionsPerGame);
